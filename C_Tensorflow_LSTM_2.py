08e7a6e62222eee9b6dc9001949e22bc2111d471
marshao <mars.guan@foxmail.com>
2018-05-31 17:05:13 +0800

    2018-03-13 in Master: * This is to create the baseline
    2018-03-19 in Master:
    * Creating the base API and structure of program
    
    2018-03-26 in Master:
    * Keep Coding
    
    2018-03-29 in Master:
    * Almost finished fund_code
    
    2018-04-02 in Master:
    * Almist finished the fund_infor
    
    2018-04-08 in Master:
    * Fund Manager Infor
    
    2018-04-08 in Master:
    * Fund Net Value
    
    2018-04-12 in Master:
    *Build Class Fund
    
    2018-04-13 in Master:
    *Stuck in how to extract year number from a series of Pandas for getlongerthan function.
    
    2018-04-16 in Master:
    *Get into Fund Analysis phase.
    
    2018-04-16 in Master:
    * Change Pycharm enviroment
    * FundsAnalysis of Correlation Matrix
    
    2018-04-18 in Master:
    * Added Satistics functions
    * Added Heatmap functions
    * Added daily_chg ananlysis
    
    2018-04-19 in Master:
    * Added C_Table_Creation with SqlAlchemy object orientend solution
    * Added function to get fund cumulative income rates in different periods.
    
    2018-04-20 in Master:
    *Added Fund_Rank_In_Class function
    
    2018-4-26:
    * Huge improvement on the Dynamic Insert/Upsert
    
    2018-05-02:
    * Foreign Key constraint error solved
    
    2018-05-03:
    * PostionDetail
    *HolderChange
    
    2018-05-06:
    # Fund Yearly Industry Config
    # Chinese charactor encoding error
    
    #2018-05-07:
    # Get indices history data from Yahoo with proxy
    
    2018-05-11:
    # Moving into fund filtering process
    
    2018-05-15:
    # filter funds
    
    2018-05-17:
    # Funds filters
    
    2018-05-21
    
    2018-05-22
    
    2018-05-23
    
    2018-05-24:
    # almost finish the data preprocessing
    # getSampleDegrouped should be modified to run multiple samples
    
    2018-05-25:
    * Data Pre Processing finished.
    
    2018-05-28:
    * Importing the first Tensorflow NN to train
    
    2018-05-30:
    * Tensorflow estimator DNN is done
    * Started to do LSTM
    
    2018-05-31:
    * Working on LSTM

--- C_Tensorflow_LSTM_2.py
+++ C_Tensorflow_LSTM_2.py
@@ -23,12 +23,12 @@ def getFeatures(samples):
 
     array_z = np.zeros((1, 395), dtype=np.float32)
     feature_name = samples[0].columns.tolist()
-    # feature_name = feature_name * 5
+    feature_name = feature_name * 5
     for i in range(0, len(feature_name)):
         feature_name[i] = '{}_{}'.format(feature_name[i], (i + 1))
 
+    features_data = pd.DataFrame()
     sample_list = []
-    keys = []
     i = 0
     # print len(samples)
     # df_lstm_samples =
@@ -41,16 +41,16 @@ def getFeatures(samples):
             sample = pd.concat([sample, df])
         if em_rows < 0:
             sample = sample.iloc[1:, :]
-        print sample.values.tolist()
-        break
-        sample_list.append(sample)
-        keys.append('week{}'.format(i))
-        i += 1
 
-    features_data = pd.concat(sample_list)
+        if array_z[0, 0] == 0:
+            array = np.array(sample.values)
+            array_z = np.reshape(array, (1, -1))
+        else:
+            array = np.array(sample.values)
+            array_z = np.vstack((array_z, np.reshape(array, (1, -1))))
 
-    # features_data = pd.DataFrame(array_z, columns=feature_name)
-    print features_data.iloc[:, :3]
+    features_data = pd.DataFrame(array_z, columns=feature_name)
+
     return features_data, feature_name
 
 
@@ -78,11 +78,11 @@ def getTFDataSets(each_set):
 
 def train_input_fn(feature, label, batch_size=128, shuffle=False):
     inputs = (dict(feature), label)
-    # print type(inputs)
+    # print inputs
     dataset = tf.data.Dataset.from_tensor_slices(inputs)
     dataset = dataset.repeat().batch(batch_size)
     # print dataset
-    return dataset
+    return feature, label
 
 
 def test_input_fn(feature, label, batch_size=128, shuffle=False):
@@ -94,14 +94,15 @@ def test_input_fn(feature, label, batch_size=128, shuffle=False):
     # print type(inputs)
     dataset = tf.data.Dataset.from_tensor_slices(inputs).batch(batch_size)
 
-    return dataset
+    return feature, label
 
 
-def lstm_model_fn(feature, label, mode, time_step=None, hidden_units=None, classes=None, lr=0.0001, drop_out=0.5):
+def lstm_model_fn(features, labels, mode):
     # time_step is fund time period
     '''
     Input_size = [-1, 5, 79]
     Model V1: Single Cell LSTM RNN
+    Model V2:
     :param feature:
     :param label:
     :param mode:
@@ -112,17 +113,61 @@ def lstm_model_fn(feature, label, mode, time_step=None, hidden_units=None, class
     :param drop_out:
     :return:
     '''
-    if time_step is None:
-        time_step = 5
-    if hidden_units is None:
-        hidden_units = 79
-    if classes is None:
-        classes = 3
 
+    time_step = 5
+    hidden_units = [128, 256]
+    classes = 3
+    n_input = 79
+    drop_out = 0.4
+    lr = 0.0001
 
+    # feature = features[0]
+    # label = features[1]
 
+    X_in = tf.reshape(features, [-1, time_step, n_input])
 
+    lstm_cell_1 = tf.contrib.rnn.BasicLSTMCell(hidden_units[0], forget_bias=1.0)
 
+    lstm_cell_2 = tf.contrib.rnn.BasicLSTMCell(hidden_units[1], forget_bias=1.0)
+
+    multi_lstm_cell = tf.nn.rnn_cell.MultiRNNCell([lstm_cell_1, lstm_cell_2])
+
+    outputs, last_state = tf.nn.dynamic_rnn(cell=multi_lstm_cell, inputs=X_in, dtype=tf.float64)
+    # shape: outputs:[None, 5, 256]
+    # shape: last_state:[None, 256]
+    print last_state
+
+    dnn_1 = tf.layers.dense(inputs=last_state[1], units=1024, activation=tf.nn.relu, name='dnn_1')
+    # shape:[None, 1024]
+
+    drop_out_l = tf.layers.dropout(inputs=dnn_1, rate=drop_out, training=mode == tf.estimator.ModeKeys.TRAIN)
+
+    logits = tf.layers.dense(inputs=drop_out_l, units=3, name='output')
+
+    ### Prediction Mode
+    predictions = {
+        'classes': tf.argmax(input=logits, axis=1, name='classes'),
+        'probabilities': tf.nn.softmax(logits, name='Softmax_probabilities'),
+        'logits': logits,
+        'dnn_1': dnn_1,
+        'drop_out_l': drop_out_l
+
+    }
+
+    if mode == tf.estimator.ModeKeys.PREDICT:
+        return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
+
+    ###Training
+    loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
+    if mode == tf.estimator.ModeKeys.TRAIN:
+        optimizer = tf.train.AdamOptimizer(learning_rate=lr)
+        train_op = optimizer.minimize(loss=loss, global_step=tf.train.get_global_step())
+        return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
+
+    ### Evaluation Branch
+    eval_metric_ops = {'accuracy': tf.metrics.accuracy(labels=labels, predictions=predictions),
+                       'probabilities': predictions['prebabilities']}
+    return tf.estimator.EstimatorSpec(mode, loss, eval_metric_ops)
 
 
 def main(argv):
@@ -137,21 +182,19 @@ def main(argv):
 
     train_sets, cv_sets, test_sets = fund_data_proprocessing(beg_date, funds, df_filtered, 'Week')
     test_features_data, features_name, test_labels = getTFDataSets(test_sets)
-    # train_features_data, _, train_labels = getTFDataSets(train_sets)
-    # cv_features_data, _, cv_labels = getTFDataSets(cv_sets)
+    train_features_data, _, train_labels = getTFDataSets(train_sets)
+    cv_features_data, _, cv_labels = getTFDataSets(cv_sets)
 
     # Define Esitmaters
     feature_cols = [tf.feature_column.numeric_column(k) for k in features_name]
 
-    classifier = tf.estimator.DNNClassifier(n_classes=3, feature_columns=feature_cols, hidden_units=[1024, 512, 128],
-                                            optimizer=tf.train.AdamOptimizer(learning_rate), dropout=drop_out
-                                            )
+    # classifier = tf.estimator.Estimator(model_fn=lambda dataset, mode: lstm_model_fn(dataset, mode), model_dir="/lstm_model")
 
-    # train_op = classifier.train(input_fn=lambda:train_input_fn(train_features_data, train_labels), steps = train_steps)
+    classifier = tf.estimator.Estimator(model_fn=lstm_model_fn)
+    # params={'feature_columns':feature_cols},
+    # model_dir="/lstm_model")
 
-    # accuracy_op = classifier.evaluate(input_fn=lambda: test_input_fn(test_features_data, test_labels))
-    # accuracy_op = accuracy_op['accuracy']
-    # print("\nTest Accuracy: {0:f}%\n".format(accuracy_op*100))
+    train_op = classifier.train(input_fn=lambda: train_input_fn(train_features_data, train_labels))
 
 
 if __name__ == '__main__':
